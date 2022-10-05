f69595b23ff1424b27505c464ad6821be7e7b1fa
hogreed <pushan45hsh@gmail.com>
2022-10-05 12:27:44 +0900

    오딧 리포트 피드백 반영
    1. CRITICAL : decimal 문제, Vesting을 세팅하는 부분을 관리자 페이지 (web) 에서 진행하기 때문에 관리자 편의성을 위하여 decimal 관리는 contract 내부에서 진행하도록 구현함
    - 현재 관리자 페이지에서 setVesting 시 정수로만 넣을 수 있는 부분도 인지하고 있음.
    
    * 현재 decimal이 곱해져야 하는 부분이 누락된 부분 수정완료 ( 기능 작동 확인 )
    
    2. MAJOR : 함수 의도, setVesting 시 IERC20 표준의 transferfrom 함수를 작동하기 위한 권한 부여 - setVesting 함수를 실행하면 관리자가 토큰을 보내는 것이 아닌 contract 가 관리자의 지갑에서 권한을 승인한 만큼 꺼내 갈 수 있도록 권한을 부여하는 형태로 기능이 구현되어 있음
    
    * contract 에서 함수를 실행 할 경우 _msgSender가 contract address 가 되어서 권한을 contract address 에 부여하도록 의도한 것이 맞습니다.

--- Vesting/TavaVesting.sol
+++ Vesting/TavaVesting.sol
@@ -21,20 +21,17 @@ contract TavaVesting is ITavaVesting, Ownable, ReentrancyGuard {
         tavaTokenAddress = _tavaTokenAddress;
     }
 
-    // modifier
     modifier notZeroAddress(address caller) {
         require(caller != address(0), "Cannot perform call function for address(0).");
         _;
     }
 
-    // TAVA 잔액 조회
     function BalanceToAddress(address account) 
         external view returns(uint256)
     {
         return IERC20(tavaTokenAddress).balanceOf(account);
     }
     
-    // public
     function TokensCurrentlyReceiveable(address _receiver, uint256 _vestingIdx) 
         public view override returns(uint256 _ReciveableTokens)
     {
@@ -49,9 +46,9 @@ contract TavaVesting is ITavaVesting, Ownable, ReentrancyGuard {
             return 0;
         }
 
-        uint256 _tokensPerStage = _totalAmount*(tavaDecimal)/(_vestingCondition.unlockCnt);
+        uint256 _tokensPerStage = (_totalAmount*tavaDecimal)/(_vestingCondition.unlockCnt);
         uint256 receiveableTava = _tokensPerStage*(_elapsedDays);
-        uint256 receivedTava = sentTavasToAdr(_receiver, _vestingIdx)*(tavaDecimal);
+        uint256 receivedTava = sentTavasToAdr(_receiver, _vestingIdx);
 
         if(receiveableTava < receivedTava){
             return 0;
@@ -77,31 +74,13 @@ contract TavaVesting is ITavaVesting, Ownable, ReentrancyGuard {
     {
         return _sentTavas = vestingInfoToWallets[_receiver][_vestingIdx].tokensSent;
     }
-    // public end
 
-
-    /**
-        정상적인경우 사용할 일 없으나 혹여 이슈로 인한 대응이 가능하도록 보상받는 ERC20 주소를 변경 할 수 있도록 함.
-    */
     function setTavaAddress(address _tavaTokenAddress) 
         external onlyOwner
     {
         tavaTokenAddress = _tavaTokenAddress;
     }
 
-    /**
-        * _unlockedTokenAmount가  로 나누어 떨어지지 않으면, user는   전체를 받아가지 못_unlockCnt _unlockedTokenAmount함 
-        
-        -- 해당 부분이 어떤의미인지 알 수 없음. 나누어 떨어지지않아도 보상수령이 가능한 것을 확인함.
-        -- decimal 을 곱해서 나누어 주면 소숫점문제로 적게 받는 부분을 최소화 하여 계산하게 됨.
-        -- 나누어서 몫이 잘 떨어지는 부분은 확인되어서 문제가 없을 것으로 예상됨
-
-
-        * _StartDt 값이 (실수로) 작은 값 또는 0이 입력되면, user는 의도보다 빠르게 token을 claim할 수 있음
-
-        -- 테스트를 위하여 과거의 시간을 넣을 수 있도록 되어있는 상태
-        -- 관리자가 프론트단에서 date picker 를통해 넣을 것이기 때문에 문제가 없을 것으로 예상됨
-    */
     function setVesting(
         address _receiver, 
         uint256 _unlockedTokenAmount, 
@@ -140,13 +119,6 @@ contract TavaVesting is ITavaVesting, Ownable, ReentrancyGuard {
         emit canceledVesting(_receiver, _vestingIdx);
     }
 
-
-    /**
-        * approvalTava()의 작성 의도를 이해하기 힘듬
-
-        -- IERC20(tavaTokenAddress).transferFrom(_msgSender(), address(this), AmountToReceived); 해당부분 L122 에서 함수 실행시 필수적으로 권한이 부여되어야함
-        -- 관리자의 토큰을 프론트 플로우로 contract 에 전달하기 위하여 필요함
-    */
     function approvalTava(uint256 _amount) 
         external override 
     {
@@ -156,24 +128,23 @@ contract TavaVesting is ITavaVesting, Ownable, ReentrancyGuard {
     function claimVesting(uint256 _vestingIdx) 
         external override notZeroAddress(_msgSender()) nonReentrant returns(uint256 _TokenPayout)
     {
-        require(vestingInfoToWallets[_msgSender()][_vestingIdx].valid, "claimVesting_ERR01");   // 취소된 베스팅인지 확인
+        require(vestingInfoToWallets[_msgSender()][_vestingIdx].valid, "claimVesting_ERR01"); 
         
         uint256 _elapsedDays = getElapsedDays(_msgSender(), _vestingIdx);
         
-        require(_elapsedDays > 0, "claimVesting_ERR02"); // 경과시간이 duration 을 최초 1번 지난 경우 경과일(days 단위 표시)
+        require(_elapsedDays > 0, "claimVesting_ERR02");
         
-        uint256 _tokensSent = sentTavasToAdr(_msgSender(), _vestingIdx); // 단위 ether
-        uint256 _totalAmount = vestingInfoToWallets[_msgSender()][_vestingIdx].totalAmount; // 단위 ether
+        uint256 _tokensSent = sentTavasToAdr(_msgSender(), _vestingIdx);
+        uint256 _totalAmount = vestingInfoToWallets[_msgSender()][_vestingIdx].totalAmount*tavaDecimal;
         
         require(_totalAmount > _tokensSent, "claimVesting_ERR03");
         
-        uint256 _currentAmount = TokensCurrentlyReceiveable(_msgSender(), _vestingIdx); // 단위 wei
+        uint256 _currentAmount = TokensCurrentlyReceiveable(_msgSender(), _vestingIdx); 
         
         require(_currentAmount > 0, "claimVesting_ERR04");
 
         IERC20(tavaTokenAddress).transfer(_msgSender(), _currentAmount);
 
-        //uint256 _currentAmountToTava = _currentAmount/(tavaDecimal); // 단위 ether
         vestingInfoToWallets[_msgSender()][_vestingIdx].tokensSent += _currentAmount;
         TotalTokensReceived += _currentAmount;
         emit claimedVesting(_msgSender(), _vestingIdx, _currentAmount, block.timestamp);
